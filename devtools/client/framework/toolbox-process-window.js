b303165b1b640da1d5906e8a8abfaf9762eded08
Alexandre Poirot <poirot.alex@gmail.com>
2016-01-18 03:10:42 -0800

    Bug 950936 - Add disable autohide button in browser toolbox to help debug panel/popups. r=bgrins

--- devtools/client/framework/toolbox-process-window.js
+++ devtools/client/framework/toolbox-process-window.js
@@ -56,6 +56,7 @@ function setPrefDefaults() {
   Services.prefs.setBoolPref("devtools.inspector.showAllAnonymousContent", true);
   Services.prefs.setBoolPref("browser.dom.window.dump.enabled", true);
   Services.prefs.setBoolPref("devtools.scratchpad.enabled", true);
+  Services.prefs.setBoolPref("devtools.command-button-noautohide.enabled", true);
 }
 
 window.addEventListener("load", function() {
