9cb002bc953bfa480beab863f3a1e2e7e6297d83
Alexandre Poirot <poirot.alex@gmail.com>
2016-03-17 10:10:54 -0700

    Bug 1257525 - Watch addon files to hotreload CSS files. r=jryans

--- devtools/client/aboutdebugging/components/addons/controls.js
+++ devtools/client/aboutdebugging/components/addons/controls.js
@@ -10,11 +10,19 @@
 loader.lazyImporter(this, "AddonManager",
   "resource://gre/modules/AddonManager.jsm");
 
-const { Cc, Ci } = require("chrome");
+const { Cc, Ci, components } = require("chrome");
 const { createFactory, createClass, DOM: dom } =
   require("devtools/client/shared/vendor/react");
 const Services = require("Services");
 const AddonsInstallError = createFactory(require("./install-error"));
+
+loader.lazyRequireGetter(this, "watchFiles", "devtools/client/shared/file-watcher", true);
+loader.lazyImporter(this, "OS", "resource://gre/modules/osfile.jsm");
+loader.lazyImporter(this, "NetUtil", "resource://gre/modules/NetUtil.jsm");
+loader.lazyImporter(this, "FileUtils", "resource://gre/modules/FileUtils.jsm");
+loader.lazyGetter(this, "DOMUtils", () => {
+  return Cc["@mozilla.org/inspector/dom-utils;1"].getService(Ci.inIDOMUtils);
+});
 
 const Strings = Services.strings.createBundle(
   "chrome://devtools/locale/aboutdebugging.properties");
@@ -58,6 +66,11 @@ module.exports = createClass({
       .catch(e => {
         console.error(e);
         this.setState({ installError: e.message });
+      })
+      .then(() => {
+        if (file.isDirectory()) {
+          this.tryHookingHotreload(file);
+        }
       });
   },
 
@@ -89,5 +102,148 @@ module.exports = createClass({
         }, Strings.GetStringFromName("loadTemporaryAddon"))
       ),
       AddonsInstallError({ error: this.state.installError }));
-  }
+  },
+
+  tryHookingHotreload(folder) {
+    let manifest = folder.clone();
+    manifest.append("chrome.manifest");
+    if (!manifest.exists()) {
+      return;
+    }
+    this.readFile(manifest.path).then(data => {
+      let mappings = {};
+      let lines = data.split(/\n/);
+      let entry = /^\s*(content|resource|skin)\s+(\S+)\s+(\S+)\s*(\S+)?\s*/;
+      for (let line of lines) {
+        let match = entry.exec(line);
+        if (match) {
+          let type = match[1];
+          let name = match[2];
+          let path = match[3];
+          let url;
+          if (type == "content") {
+            url = "chrome://" + name + "/content";
+          } else if (type == "skin") {
+            url = "chrome://" + name + "/skin";
+            // match[3] ends up being the skin name
+            path = match[4];
+          } else if (type == "resource") {
+            url = "resource://" + name;
+          } else {
+            continue;
+          }
+          mappings[url] = OS.Path.normalize(OS.Path.join(folder.path, path));
+        }
+      }
+      dump("Addon file mapping: " + JSON.stringify(mappings, null, 2)+"\n");
+      this.watchAndReload(mappings).start();
+    });
+  },
+
+  watchAndReload(mappings) {
+    let stylesheets = {};
+
+    let onFileUpdated = path => {
+      dump("File updated: "+path+"\n");
+      let stylesheet = stylesheets[path];
+      if (stylesheet) {
+        this.readFile(path).then(data => {
+          DOMUtils.parseStyleSheet(stylesheet, data);
+          dump("CSS updated.\n");
+        });
+      }
+    };
+
+    for (let prefix in mappings) {
+      stylesheets[prefix] = [];
+      let path = mappings[prefix];
+      watchFiles(path, onFileUpdated);
+    }
+
+    let listener = event => {
+      let url = event.stylesheet.href;
+      if (!url) return;
+      dump("New style sheet from: "+event.target.location+" url="+url+"\n");
+      for (let prefix in mappings) {
+        if (url.startsWith(prefix)) {
+          let path = url.replace(prefix, mappings[prefix]);
+          // Normalize path on windows:
+          if (Services.appinfo.OS == "WINNT") {
+            path = path.replace(/\//g, "\\");
+          }
+          dump("style sheet available at: "+path+"\n");
+          stylesheets[path] = event.stylesheet;
+        }
+      }
+    };
+
+    let observer = function (window, topic, data) {
+      dump("New top level document: "+window.location+" topic:"+topic+"\n");
+      if (topic === "domwindowopened" ||
+          topic === "chrome-document-global-created" ||
+          topic == "load") {
+        // about:blank document may be replaced with a new document without firing a new
+        // chrome-document-global-created
+        if (!window.location.href || window.location == "about:blank") {
+          window.addEventListener("DOMContentLoaded", function onLoad() {
+            window.removeEventListener("DOMContentLoaded", onLoad);
+            observer(window, "load", null);
+          }, true);
+          return;
+        }
+        // Register already loaded stylesheets
+        for (let s of window.document.styleSheets) {
+          listener({stylesheet: s, target:window.document});
+        }
+        // But also listen for new ones
+        window.document.styleSheetChangeEventsEnabled = true;
+        window.addEventListener("StyleSheetAdded", listener, true);
+      } else {
+        window.document.styleSheetChangeEventsEnabled = false;
+        window.removeEventListener("StyleSheetAdded", listener);
+      }
+    };
+
+    return {
+      start: function () {
+        // Automatically process already opened windows
+        let e = Services.ww.getWindowEnumerator();
+        while (e.hasMoreElements()) {
+          let window = e.getNext();
+          observer(window, "domwindowopened", null);
+        }
+        // And listen for new ones to come
+        Services.ww.registerNotification(observer);
+        Services.obs.addObserver(observer, "chrome-document-global-created", false);
+      },
+
+      stop: function () {
+        Services.ww.unregisterNotification(observer);
+        Services.obs.removeObserver(observer, "chrome-document-global-created", false);
+        let e = Services.ww.getWindowEnumerator();
+        while (e.hasMoreElements()) {
+          let window = e.getNext();
+          observer(window, "domwindowclosed", null);
+        }
+      }
+    };
+  },
+
+  readFile(path) {
+    return new Promise((resolve, reject) => {
+      let file = new FileUtils.File(path);
+      NetUtil.asyncFetch({
+        uri: NetUtil.newURI(file),
+        loadUsingSystemPrincipal: true
+      }, (inputStream, status) => {
+        if (!components.isSuccessCode(status)) {
+          reject(new Error("Couldn't load manifest: " + filename + "\n"));
+          return;
+        }
+        var data = NetUtil.readInputStreamToString(inputStream, inputStream.available());
+        resolve(data);
+      });
+    });
+  },
+
 });
